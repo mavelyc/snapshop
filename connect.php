7eb563d75c436add08b207564017f35331288ff3
mavelyc <35879502+mavelyc@users.noreply.github.com>
2018-09-15 16:29:51 -0400

    finished all bios and added client pngs

--- connect.php
+++ connect.php
@@ -1,25 +1,17 @@
 <?php
-if (!empty($_GET['location'])) {
-    /**
-     * Here we build the url we'll be using to access the google maps api
-     */
-    $maps_url = 'https://' .
-        'maps.googleapis.com/' .
-        'maps/api/geocode/json' .
-        '?address=' . urlencode($_GET['location']);
+     
+    $maps_url = 'http://localhost:8001/application/json'
     $maps_json = file_get_contents($maps_url);
     $maps_array = json_decode($maps_json, true);
-    $lat = $maps_array['results'][0]['geometry']['location']['lat'];
-    $lng = $maps_array['results'][0]['geometry']['location']['lng'];
+    $logo = $maps_array['results'][0]['logoAnnotations'][0]['description']
+    $label_0 = $maps_array['results'][0]['labelAnnotations'][0]['description']
+    $label_1 = $maps_array['results'][0]['labelAnnotations'][1]['description']
+    $label_2 = $maps_array['results'][0]['labelAnnotations'][2]['description']
     /**
      * Time to make our Instagram api request. We'll build the url using the
      * coordinate values returned by the google maps api
      */
-    $url = 'https://' .
-        'api.instagram.com/v1/media/search' .
-        '?lat=' . $lat .
-        '&lng=' . $lng .
-        '&client_id=CLIENT-ID'; //replace "CLIENT-ID"
+    $url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyB5UrKLWTvJMk3U2yW_gXdoYpxZ35BHuIE&cx=005213571837044908416:5_5e-kd0w6i&q='. $logo; //replace "CLIENT-ID"
     $json = file_get_contents($url);
     $array = json_decode($json, true);
 }
@@ -28,16 +20,11 @@ if (!empty($_GET['location'])) {
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <title>geogram</title>
+    <title>SnapShop</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="script.js"></script>
 </head>
 <body>
-<form action="" method="get">
-    <input type="text" name="location"/>
-    <button type="submit">Submit</button>
-</form>
-<br/>
 <div id="results" data-url="<?php if (!empty($url)) echo $url ?>">
     <?php
     if (!empty($array)) {
